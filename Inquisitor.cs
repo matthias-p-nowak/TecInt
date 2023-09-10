5c016ef52e784e1f11faad4f36811418eebe0604
Matthias (generalsjefutvikler) Nowak <matthiaspeter.nowak@centric.eu>
2023-09-10 18:15:22 +0200

    initial setting

---
+++ Inquisitor.cs
@@ -0,0 +1,22 @@
+﻿internal partial class Inquisitor
+{
+    static void Main(string[] args)
+    {
+        var programType = typeof(Inquisitor);
+        foreach (var runMethod in args)
+        {
+            var method = programType.GetMethod(runMethod);
+            if (method == null)
+            {
+                Console.WriteLine($"can't find {runMethod}");
+                continue;
+            }
+            if (!method.IsStatic)
+            {
+                Console.WriteLine($"mark {method.Name} as static!");
+                continue;
+            }
+            method.Invoke(null, new object[] { });
+        }
+    }
+}
---
+++ helloworld.cs
@@ -0,0 +1,15 @@
+partial class Inquisitor
+{
+    public static void hello()
+    {
+        Console.WriteLine("hello world");
+    }
+    public static void joke()
+    {
+        int c = 10;
+        if (c++ < c)
+            Console.WriteLine("yes");
+        else
+            Console.WriteLine("no");
+    }
+}
